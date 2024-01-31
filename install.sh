f9cca80014a2d9497a59a48d69ed04c386fca5cf
Peter Thesling <peter.thesling@shopify.com>
2024-01-31 17:01:53 -0500

    Move installation into installation file

--- install.sh
+++ install.sh
@@ -37,3 +37,17 @@ sudo apt-get update
 
 sudo apt-get install -y neovim
 echo "--- Neovim installed"
+
+# Installing Vim plug
+curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Installing FZF
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --key-bindings --update-rc
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+sudo rm -rf /usr/local/bin/fzf
+sudo rm -rf /usr/bin/fzf
+
+# Install plugins
+nvim -c 'PlugInstall | qa'
